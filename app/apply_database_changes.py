0efd87409d3ed437699ea86cfcb3cebaaf360eae
Sune Kirkeby <mig@ibofobi.dk>
2013-02-25 19:16:37 +0100

    Script deployments.

--- app/apply_database_changes.py
+++ app/apply_database_changes.py
@@ -8,7 +8,7 @@ import os
 import sys
 from sqlalchemy import Column, String, DateTime
 
-from database import Session, Base, engine
+from .database import Session, Base, engine
 
 here = os.path.dirname(__file__)
 
